5fb2b380c58deaa04cb1ac768890c681b1ca4213
SuryaSankarDSC <suryasankar.dsc@gmail.com>
2024-08-11 17:52:33 +0530

    second commit

--- src/Pages/Calculation/CalculationPage.jsx
+++ src/Pages/Calculation/CalculationPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './CalculationPage.css';
+import Navbar from '../Navbar/Navbar'
 
 const CalculationPage = () => {
   const [calculationData, setCalculationData] = useState({
